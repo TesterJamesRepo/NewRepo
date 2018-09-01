f3ee5a3776f3231e19c60cee0ca3dad40a7b428a
TesterJamesRepo <42884592+TesterJamesRepo@users.noreply.github.com>
2018-09-01 11:57:19 +0530

    changes done by nilesh

--- src/test/java/com/qa/tests/FirstDemo.java
+++ src/test/java/com/qa/tests/FirstDemo.java
@@ -5,6 +5,8 @@ public class FirstDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Demo");
+		System.out.println("Second Demo");
+		System.out.println("Third Demo");
 
 	}
 
