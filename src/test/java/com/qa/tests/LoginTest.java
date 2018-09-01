185a9e086e35589f3d104f840b2a52c790e9221e
testerjames007 <testerjames007@gmail.com>
2018-09-01 12:13:39 +0530

    login branch commit

---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,20 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LoginTest {
+  @Test
+  public void f() {
+	  //login code
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
