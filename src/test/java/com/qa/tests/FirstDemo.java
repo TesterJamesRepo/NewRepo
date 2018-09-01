0cc92cbce6ba88ff5a37f46c18d2745391f1236f
testerjames007 <testerjames007@gmail.com>
2018-09-01 12:07:07 +0530

    commit changes second

--- src/test/java/com/qa/tests/FirstDemo.java
+++ src/test/java/com/qa/tests/FirstDemo.java
@@ -5,9 +5,12 @@ public class FirstDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Demo");
-		System.out.println("Second Demo");
+		
 		System.out.println("Third Demo");
+		System.out.println("forth Demo");
+		System.out.println("fifth Demo");
 
 	}
-
+	
+	
 }
